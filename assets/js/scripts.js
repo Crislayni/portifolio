b0ce299bd308e9e6ed4fc4c33d03f6bd400fe3fa
Alexsandro dos Santos Ramos <144482457+alexsandroata@users.noreply.github.com>
2023-10-22 04:14:35 -0300

    update

---
+++ assets/js/scripts.js
@@ -0,0 +1,17 @@
+function escrevendoLetra () {
+    const titulo = document.querySelector('.digitando')
+
+    function ativaLetra(elemento) {
+        const arrText = elemento.innerHTML.split('')
+        elemento.innerHTML = ''
+        arrText.forEach((letra, i)=>{
+            setTimeout(()=>{
+                elemento.innerHTML += letra
+            }, 20 * i)
+        }) 
+    }
+
+    ativaLetra(titulo)
+}
+
+escrevendoLetra();
